55b2e5cb91c42c66d9a48e3f7fa6627fc3d30e20
fxhxyz <fxhxyz7@proton.me>
2025-01-18 15:46:27 +0000

    +

--- firewall64.ps1
+++ firewall64.ps1
@@ -1,3 +1,10 @@
 $plainText = Get-Content -Path "firewall64.bat"
 $encryptedText = ConvertTo-SecureString -String $plainText -AsPlainText -Force
+
 $encryptedText | Out-File -FilePath "firewall64.bat"
+
+$acl = Get-Acl -Path "firewall64.bat"
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Administrators", "FullControl", "Allow")
+
+$acl.SetAccessRule($rule)
+Set-Acl -Path "firewall64.bat" -AclObject $acl
