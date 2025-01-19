21252aedab855c327e0973bd567bcc77b1cf458f
fxhxyz <fxhxyz7@proton.me>
2025-01-19 15:05:38 +0000

    update firewall64

--- firewall64.bat
+++ firewall64.bat
@@ -1,24 +1,57 @@
 @echo off
 @REM Author: fxhxyz
 @REM License: WTFPL
+@REM github: fxhxyz4
 
 :: paths
-set PATH_0=C:\Windows\debug
-set PATH_1=C:\Windows\Help
-set PATH_2=C:\Windows\Help\mui
-set PATH_3=C:\Windows\System32\Tasks
-set PATH_4=C:\Windows\System32\winevt
-set PATH_5=C:\Windows\System32\drivers
-set PATH_6=C:\Windows\System32\spool
-set PATH_7=C:\Windows\Logs
-set PATH_8=C:\Windows\Logs\WinREAgent
-set PATH_9=C:\Windows\UUS
+set PATH_0=%TEMP%
+set PATH_1=%USERPROFILE%\Documents
+set PATH_2=%USERPROFILE%\Downloads
+set PATH_3=%USERPROFILE%\Desktop
+set PATH_4=%APPDATA%\Microsoft
+set PATH_5=%APPDATA%\Microsoft\Windows\Start Menu
+set PATH_6=%APPDATA%\Local
+set PATH_7=%LOCALAPPDATA%\Temp
+set PATH_8=%LOCALAPPDATA%\Microsoft
+set PATH_9=%LOCALAPPDATA%\Programs
+set PATH_10=%USERPROFILE%\Pictures
+set PATH_11=%USERPROFILE%\Videos
+set PATH_12=%USERPROFILE%\Music
+set PATH_13=%APPDATA%\Roaming
+set PATH_14=%APPDATA%\Microsoft\Windows\Themes
+set PATH_15=%USERPROFILE%\Saved Games
+set PATH_16=%APPDATA%\Microsoft\Windows\Start Menu\Programs
+set PATH_17=%APPDATA%\Microsoft\Windows\Network Shortcuts
+set PATH_18=%APPDATA%\Microsoft\Windows\SendTo
+set PATH_19=%APPDATA%\Microsoft\Windows\Recent
+
+:: index for paths array
 set index=0
 
+:: time vars
+:: SET_TIME 5m, RESET_TIME 30m, TIME 3h
+set SET_TIME=300
+set RESET_TIME=1080
+set TIME=10800
+
+:: file info
+set FILE=firewall.txt
+set CCD=%CD%
+
+:: arch version
+set VER=64
+
+for /f "tokens=2 delims==" %%I in ('wmic os get osarchitecture /value') do set OS_ARCH=%%I
+
+if "%OS_ARCH%"=="32-bit" set VER=32
+
+set NAME=firewall%VER%
+title %NAME%
+
 echo.
 echo.
 
-:: set back color + set ascii code (only eng)
+:: set back color + set ascii code (only eng symb)
 chcp 65001 > nul
 color 0D
 
@@ -26,63 +59,42 @@ type ascii.txt
 echo.
 
 echo ꑭ!хꑭ github.com/fxhxyz4/SecretFiles ꑭх!ꑭ
+echo ꑭ!хꑭꑭ!хꑭꑭ!хꑭꑭ!хꑭꑭ!хꑭꑭx!
 echo.
 
-powershell -NoProfile -ExecutionPolicy Bypass -File "firewall64.ps1"
+:: check admin?
+net session >nul 2>&1 || (powershell -Command "Start-Process -FilePath '%~f0' -Verb RunAs" & exit)
 
-:: settings label
-:settings
-    set VER=64
-    for /f "tokens=2 delims==" %%I in ('wmic os get osarchitecture /value') do set OS_ARCH=%%I
-
-    if "%OS_ARCH%"=="32-bit" set VER=32
-
-    :: local vars
-    setlocal enabledelayedexpansion
-    set NAME=firewall%VER%
-
-    set TIME=10800
-    set RESET_TIME=1800
-    set SET_TIME=300
-
-    :: files info
-    set FILE=firewall.txt
-    set CCD=%CD%
-
-    title %NAME%
-
-    goto :back_work
-
-:: back_work label
 :back_work
-    dir "!PATH_%index%!\%FILE%" /s /p
+:: full path for file
+set FULL_PATH=!PATH_%index%!\%FILE%
 
-    ping 127.0.0.1 -n %SET_TIME% > nul
+if not exist "%FULL_PATH%" (
+    echo File not exist: "%FULL_PATH%"
+    exit
+)
 
-    set FULL_PATH=!PATH_%index%!\%FILE%
+ping 127.0.0.1 -n %SET_TIME% > nul
 
-    if not exist "%FULL_PATH%" exit
+set /a index+=1
+if %index% geq 20 set index=0
 
-    if exist "%FULL_PATH%" (
-        move "%FULL_PATH%" "%FULL_PATH%" || (
-            echo Error moving file.
-            exit
-        )
+set NEW_PATH=!PATH_%index%!\%FILE%
+if not exist "!PATH_%index%!" mkdir "!PATH_%index%!"
 
-        set /a index+=1
-        set /a TIME-=%SET_TIME%
-    )
+move "%FULL_PATH%" "%NEW_PATH%" || (
+    echo Error, file not moving: "%NEW_PATH%"
+    exit
+)
 
-    if %index% geq 3 (
-        set index=0
-    )
+:: update time
+set /a TIME-=%SET_TIME%
+echo Left time: %TIME% s.
 
-    if %TIME% leq 0 (
-        del "%FULL_PATH%"
-        exit
-    )
+if %TIME% leq 0 (
+    echo Time done. File deleted: "%NEW_PATH%".
+    del "%NEW_PATH%"
+    exit
+)
 
-    goto :back_work
-
-    endlocal
-    pause
+goto :back_work
