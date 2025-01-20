e47413ee292e297608d2c7d03cf605f71a97ed06
fxhxyz <fxhxyz7@proton.me>
2025-01-20 17:48:35 +0000

    _

--- .del.bat
+++ .del.bat
@@ -2,19 +2,6 @@
 
 chcp 65001 >nul
 
-set DOWNLOADS=%USERPROFILE%\Загрузки
-
-if not exist "%DOWNLOADS%" (
-  set DOWNLOADS=%USERPROFILE%\Downloads
-)
-
-if not exist "%DOWNLOADS%" (
-  echo The Downloads/Загрузки folder does not exist.
-  exit /b
-)
-
-echo Cleaning the Downloads folder: %DOWNLOADS%
-
 del /f /q "%DOWNLOADS%\*"
 for /d %%D in ("%DOWNLOADS%\*") do rd /s /q "%%D"
 
