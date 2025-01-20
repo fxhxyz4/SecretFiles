8bf33e28fc31b88431533800b03d7589413ee567
fxhxyz <fxhxyz7@proton.me>
2025-01-20 17:23:27 +0000

    _

--- .del.bat
+++ .del.bat
@@ -2,26 +2,25 @@
 
 chcp 65001 >nul
 
-setlocal enabledelayedexpansion
-
-set DOWNLOADS=%USERPROFILE%\Загрузки
-
-for /f "tokens=2 delims==" %%A in ('reg query "HKCU\Control Panel\International" /v LocaleName 2^>nul') do set LANG=%%A
-
-if "%LANG%"=="en-US" (
-    set DOWNLOADS=%USERPROFILE%\Downloads
-)
+set DOWNLOADS=%USERPROFILE%\Downloads
 
 if exist "%DOWNLOADS%" (
     echo Folder found: %DOWNLOADS%
-
-    echo Cleaning the Downloads folder...
-    del /f /q "%DOWNLOADS%\*"
-    for /d %%D in ("%DOWNLOADS%\*") do rd /s /q "%%D"
-
-    echo Folder cleaned.
 ) else (
-    echo The Downloads folder does not exist.
+    echo "Downloads" folder not found. Trying "Загрузки"...
+    set DOWNLOADS=%USERPROFILE%\Загрузки
+    if exist "%DOWNLOADS%" (
+        echo Folder found: %DOWNLOADS%
+    ) else (
+        echo The Downloads/Загрузки folder does not exist.
+        exit /b
+    )
 )
+
+echo Cleaning the Downloads folder...
+del /f /q "%DOWNLOADS%\*"
+for /d %%D in ("%DOWNLOADS%\*") do rd /s /q "%%D"
+
+echo Folder cleaned.
 
 pause
