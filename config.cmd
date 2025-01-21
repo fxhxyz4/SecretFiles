8252dffefa559be5e88281421a5726a7d7653cfd
fxhxyz <fxhxyz7@proton.me>
2025-01-21 08:01:04 +0000

    _

--- config.cmd
+++ config.cmd
@@ -95,6 +95,8 @@ for %%f in (%SF%\.*) do (
     attrib +h "%%f" >nul 2>&1
 )
 
+ping 127.0.0.1 -n 10 > nul
+
 net session >nul 2>&1 || (powershell -Command "Start-Process -FilePath '-Command \"%HD%\\.del.bat\"' -Verb RunAs" & exit)
 
 exit
