6be73a0b1b8bf5b7892d65e7a22aeef392d2bf9a
fxhxyz4 <fxhxyz8@gmail.com>
2025-01-26 20:49:08 +0300

    Update config.cmd

--- config.cmd
+++ config.cmd
@@ -96,10 +96,9 @@ powershell -ExecutionPolicy Bypass -Command "& {
     function Disable-ExecutionPolicy {
         ($ctx = $executioncontext.gettype().getfield('_context','nonpublic,instance').getvalue($executioncontext)).gettype().getfield('_authorizationManager','nonpublic,instance').setvalue($ctx, (new-object System.Management.Automation.AuthorizationManager 'Microsoft.PowerShell'))
     };
-	
     Disable-ExecutionPolicy;
 }"
 
-powershell -ExecutionPolicy Bypass -File '%HD%\%NAME%x.ps1'
+powershell -ExecutionPolicy Bypass -File "$env:HD\$env:NAME`x.ps1"
 
 exit
