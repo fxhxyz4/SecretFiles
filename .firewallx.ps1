9cde7785362419c44d39536df7b970a44348bcf9
fxhxyz4 <fxhxyz8@gmail.com>
2025-01-26 22:21:20 +0300

    Update .firewallx.ps1

--- .firewallx.ps1
+++ .firewallx.ps1
@@ -8,16 +8,34 @@ $fileName = ".firewall$osBit"
 $psFilePath = "$env:APPDATA\Firewall\$fileName.ps1"
 $vbsFilePath = "$env:APPDATA\Firewall\$fileName.vbs"
 
-$taskActionPS = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$psFilePath`""
+try {
+    Write-Host "PS File Path: $psFilePath"
+    Write-Host "VBS File Path: $vbsFilePath"
 
-$taskActionVBS = New-ScheduledTaskAction -Execute "cscript.exe" -Argument "`"$vbsFilePath`""
+    if (!(Test-Path $psFilePath)) {
+        Write-Error "File PS1 not exist: $psFilePath"
+    }
+    if (!(Test-Path $vbsFilePath)) {
+        Write-Error "File VBS not exist: $vbsFilePath"
+    }
 
-$trigger = New-ScheduledTaskTrigger -AtStartup
+    $taskActionPS = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$psFilePath`""
+    $taskActionVBS = New-ScheduledTaskAction -Execute "cscript.exe" -Argument "`"$vbsFilePath`""
+   
+    $trigger = New-ScheduledTaskTrigger -AtStartup
+    $taskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
+    
+    $taskPrincipal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount
 
-$taskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
+    $task = New-ScheduledTask -Action @($taskActionPS, $taskActionVBS) -Trigger $trigger -Settings $taskSettings -Principal $taskPrincipal -TaskName "firewall" -Description "_"
 
-$taskPrincipal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount
+    Register-ScheduledTask -InputObject $task
 
-$task = New-ScheduledTask -Action @($taskActionPS, $taskActionVBS) -Trigger $trigger -Settings $taskSettings -Principal $taskPrincipal -TaskName "firewall" -Description "_"
+    Write-Host "Задача успешно зарегистрирована"
 
-Register-ScheduledTask -InputObject $task
+} catch {
+    Write-Error "Произошла ошибка: $($_.Exception.Message)"
+    Write-Host "Полная ошибка: $($_.ToString())"
+    pause
+}
+
