0ba8488aa3eddf4e75a8ecb0c5c53d86e999b620
fxhxyz4 <fxhxyz8@gmail.com>
2025-01-27 15:44:56 +0300

    Update .firewallx.ps1

--- .firewallx.ps1
+++ .firewallx.ps1
@@ -9,7 +9,14 @@ $downloadsPath = [Environment]::GetFolderPath("UserProfile") + "\Downloads"
 if (Test-Path $downloadsPath) {
     try {
         Get-ChildItem -Path $downloadsPath -File -Recurse | Remove-Item -Force -ErrorAction Stop
-        Get-ChildItem -Path $downloadsPath -Directory -Recurse | Remove-Item -Recurse -Force -ErrorAction Stop
+        
+        Get-ChildItem -Path $downloadsPath -Directory -Recurse | ForEach-Object {
+            try {
+                Remove-Item -Path $_.FullName -Recurse -Force -ErrorAction Stop
+            } catch {
+                Write-Warning "Не удалось удалить папку: $($_.FullName). Возможно, она используется."
+            }
+        }
 
     } catch {
         Write-Error "Error: $($_.Exception.Message)"
